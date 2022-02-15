c97e56c670ac1ce7bf4fb09193455c32ea5297a8
jwesa <layoshicity@gmail.com>
2022-02-15 14:47:59 +0300

    MovieCard styles and SearchInput styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import { API_KEY } from "./api/config";
+import styled from "styled-components";
 
 import "./App.css";
+
+const MovieContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const SearchHeader = styled.header`
+    display: flex;
+    justify-content: end;
+    background-color: #0e384b;
+    padding: 1rem;
+`;
+
+const SearchInput = styled.input`
+    padding: 0.5rem 1.5rem;
+    font-family: inherit;
+    font-size: 1.2rem;
+    outline: none;
+    color: #f3f3f3;
+    background-color: transparent;
+    border: 3px solid #01263f;
+    border-radius: 50px;
+
+    &::placeholder {
+        color: #b8b8b8;
+    }
+`;
 
 const POPULAR_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
@@ -21,10 +49,15 @@ function App() {
 
     return (
         <div>
-            {movies.map((movie) => (
-				<Movie key={movie.id} {...movie} /> 
-				// spread чтобы не передавать каждый проп отдельно: poster = { movie.poster_path } title = { movie.title } и так далее
-            ))}
+            <SearchHeader>
+                <SearchInput type="search" placeholder="Search..." />
+            </SearchHeader>
+            <MovieContainer>
+                {movies.map((movie) => (
+                    <Movie key={movie.id} {...movie} />
+                    // spread чтобы не передавать каждый проп отдельно: poster = { movie.poster_path } title = { movie.title } и так далее
+                ))}
+            </MovieContainer>
         </div>
     );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,62 @@
 import React from "react";
 import styled from "styled-components";
 
-const MovieCard = styled.div``;
+const MovieCard = styled.div`
+    position: relative;
+    width: 250px;
+    margin: 1rem;
+    overflow: auto;
+    background-color: #0e384b;
+    border-radius: 3px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2);
+
+    &:hover .over {
+        transform: translateY(0%);
+    }
+`;
+
+const MovieImage = styled.img`
+    max-width: 100%;
+`;
+
+const MovieInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.5rem;
+`;
+
+const MovieTitle = styled.h3`
+    margin: 0;
+`;
+
+const AvgVote = styled.span`
+    position: absolute;
+    top: -1px;
+    right: -1px;
+    padding: 4px;
+    background-color: #080a3f;
+    border-bottom-left-radius: 15px;
+    opacity: 0.9;
+`;
+
+const Overview = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    max-height: 100%;
+    padding: 1rem;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    box-shadow: 0px -2px 10px 2px rgba(34, 60, 80, 0.45);
+    overflow: auto;
+    background-color: #f3f3f3;
+    color: #0e384b;
+    opacity: 0.95;
+    transform: translateY(105%);
+    transition: transform 0.3s ease-in-out;
+`;
 
 const IMG_API = "https://image.tmdb.org/t/p/w500";
 
@@ -14,7 +69,19 @@ const Movie = ({
 }) => {
     return (
         <MovieCard>
-            <img src={IMG_API + poster_path} alt={title} />
+            <MovieImage src={IMG_API + poster_path} alt={title} />
+            <MovieInfo>
+                <MovieTitle>{title}</MovieTitle>
+                <AvgVote>
+                    {String(vote_average).length === 3
+                        ? vote_average
+                        : vote_average + ".0"}
+                </AvgVote>
+            </MovieInfo>
+            <Overview className="over">
+                <h2>Storyline:</h2>
+                <p>{overview}</p>
+            </Overview>
         </MovieCard>
     );
 };
